ae46a66b5fa6fedf71cd1b0fc59e9568ff1a1f98
Burkhard Ringlein <ngl@zurich.ibm.com>
2018-04-25 14:17:06 +0000

    going back to project mode; debugging 7/XX
    Former-commit-id: 61d5e79acfc94c750d179b49b5c74d7533eac0ff

--- tcl/run_pr_old.tcl
+++ tcl/run_pr_old.tcl
@@ -120,36 +120,36 @@ if { ${gCleanXprDir} } {
 
 # Create the Xilinx project
 #-------------------------------------------------------------------------------
-if { [ file exists ${xprDir} ] != 1 } {
-    file mkdir ${xprDir}
-} else {
-    # Check if the Xilinx Project Already Exists
-    if { [ file exists ${xprDir}/${xprName}.xpr ] == 1 && ! ${force} } {
-        my_warn_puts "The project \'${xprName}.xpr\' already exists!"
-        my_warn_puts "You are about to delete the project directory: '${xprDir}\' "
-        my_warn_puts "\t Are you sure (Y/N) ? "
-        flush stdout
-        set kbdIn [ gets stdin ]
-        scan ${kbdIn} "%s" keyPressed
-        if { [ string toupper ${keyPressed} ] ne "Y" } {
-            my_puts "OK, go it. This script (\'${argv0}\') will be aborted now."
-            my_puts "Bye.\n" 
-            exit 0
-        }
-    }  
-}
+#if { [ file exists ${xprDir} ] != 1 } {
+#    file mkdir ${xprDir}
+#} else {
+#    # Check if the Xilinx Project Already Exists
+#    if { [ file exists ${xprDir}/${xprName}.xpr ] == 1 && ! ${force} } {
+#        my_warn_puts "The project \'${xprName}.xpr\' already exists!"
+#        my_warn_puts "You are about to delete the project directory: '${xprDir}\' "
+#        my_warn_puts "\t Are you sure (Y/N) ? "
+#        flush stdout
+#        set kbdIn [ gets stdin ]
+#        scan ${kbdIn} "%s" keyPressed
+#        if { [ string toupper ${keyPressed} ] ne "Y" } {
+#            my_puts "OK, go it. This script (\'${argv0}\') will be aborted now."
+#            my_puts "Bye.\n" 
+#            exit 0
+#        }
+#    }  
+#}
 
-create_project ${xprName} ${xprDir} -force
-#create_project -in_memory -part ${xilPartName} ${xprDir}/${xprName}.log -force
+#create_project ${xprName} ${xprDir} -force
+create_project -in_memory -part ${xilPartName} ${xprDir}/${xprName}.log -force
 my_dbg_trace "Done with project creation." ${dbgLvl_1}
 
 #Turn on source management for mod ref
-#set_property source_mgmt_mode All [current_project]
+set_property source_mgmt_mode All [current_project]
 
 # Set Project Properties
 #-------------------------------------------------------------------------------
-set          obj               [ get_projects ${xprName} ]
-#set obj [ current_project ]
+#set          obj               [ get_projects ${xprName} ]
+set obj [ current_project ]
 
 set_property part              ${xilPartName}       ${obj}              -verbose
 set_property "target_language" "VHDL"               ${obj}              -verbose
@@ -159,30 +159,30 @@ set_property top               ${topName}           [ current_fileset ] -verbose
 set_property top_file          ${hdlDir}/${topFile} [ current_fileset ] -verbose 
 
 set_property "default_lib"                "xil_defaultlib" ${obj}
-set_property "ip_cache_permissions"       "read write"     ${obj}
-set_property "sim.ip.auto_export_scripts" "1"              ${obj}
-set_property "simulator_language"         "Mixed"          ${obj}
-set_property "xsim.array_display_limit"   "64"             ${obj}
-set_property "xsim.trace_limit"           "65536"          ${obj}
-
-set_property "ip_output_repo" "${xprDir}/${xprName}/${xprName}.cache/ip" ${obj}
+#set_property "ip_cache_permissions"       "read write"     ${obj}
+#set_property "sim.ip.auto_export_scripts" "1"              ${obj}
+#set_property "simulator_language"         "Mixed"          ${obj}
+#set_property "xsim.array_display_limit"   "64"             ${obj}
+#set_property "xsim.trace_limit"           "65536"          ${obj}
+#
+#set_property "ip_output_repo" "${xprDir}/${xprName}/${xprName}.cache/ip" ${obj}
 
 my_dbg_trace "Done with set project properties." ${dbgLvl_1}
 
 
 # Create 'sources_1' fileset (if not found)
 #-------------------------------------------------------------------------------
-if { [ string equal [ get_filesets -quiet sources_1 ] "" ] } {
-  create_fileset -srcset sources_1
-}
+#if { [ string equal [ get_filesets -quiet sources_1 ] "" ] } {
+#  create_fileset -srcset sources_1
+#}
 
 # Set IP repository paths
 #-------------------------------------------------------------------------------
-set obj [ get_filesets sources_1 ]
-my_dbg_trace "Setting ip_repo_paths to ${ipDir}" ${dbgLvl_1}
-set_property "ip_repo_paths" "${ipDir}" ${obj}
-my_dbg_trace "Done with setting ip_repo_paths to ${ipDir}" ${dbgLvl_1}
-
+#set obj [ get_filesets sources_1 ]
+#my_dbg_trace "Setting ip_repo_paths to ${ipDir}" ${dbgLvl_1}
+#set_property "ip_repo_paths" "${ipDir}" ${obj}
+#my_dbg_trace "Done with setting ip_repo_paths to ${ipDir}" ${dbgLvl_1}
+#
 # Rebuild user ip_repo's index before adding any source files
 #-------------------------------------------------------------------------------
 update_ip_catalog -rebuild
@@ -240,24 +240,24 @@ if { ${full_src} } {
 ###PR running first synth
 # Update Compile Order
 #-------------------------------------------------------------------------------
-update_compile_order -fileset sources_1
-
-# Create 'constrs_1' fileset (if not found)
-#-------------------------------------------------------------------------------
-if { [ string equal [ get_filesets -quiet constrs_1 ] ""]} {
-  create_fileset -constrset constrs_1
-}
-
-# Add Constraints Files
-#-------------------------------------------------------------------------------
-set obj [ get_filesets constrs_1 ]
-add_files -fileset ${obj} ${xdcDir}
-set_property PROCESSING_ORDER LATE [ get_files ${xdcDir}/${xprName}_pins.xdc ]
-#OBSOLETE-20180414 set_property used_in_synthesis false [ get_files ${xdcDir}/${xprName}_timg.xdc ]
-#OBSOLETE-20180414 set_property used_in_synthesis false [ get_files ${xdcDir}/${xprName}_pins.xdc ]
-
-#OBSOLETE-20180414 set_property used_in_implementation false [get_files Shell.xdc]
-my_dbg_trace "Done with adding XDC files." ${dbgLvl_1}
+#update_compile_order -fileset sources_1
+#
+## Create 'constrs_1' fileset (if not found)
+##-------------------------------------------------------------------------------
+#if { [ string equal [ get_filesets -quiet constrs_1 ] ""]} {
+#  create_fileset -constrset constrs_1
+#}
+#
+## Add Constraints Files
+##-------------------------------------------------------------------------------
+#set obj [ get_filesets constrs_1 ]
+#add_files -fileset ${obj} ${xdcDir}
+#set_property PROCESSING_ORDER LATE [ get_files ${xdcDir}/${xprName}_pins.xdc ]
+##OBSOLETE-20180414 set_property used_in_synthesis false [ get_files ${xdcDir}/${xprName}_timg.xdc ]
+##OBSOLETE-20180414 set_property used_in_synthesis false [ get_files ${xdcDir}/${xprName}_pins.xdc ]
+#
+##OBSOLETE-20180414 set_property used_in_implementation false [get_files Shell.xdc]
+#my_dbg_trace "Done with adding XDC files." ${dbgLvl_1}
 
 my_puts "################################################################################"
 my_puts "##  DONE WITH PROJECT CREATION "
@@ -271,18 +271,18 @@ my_puts "End at: [clock format [clock seconds] -format {%T %a %b %d %Y}] \n"
 
 # Create 'synth_1' run (if not found)
 #-------------------------------------------------------------------------------
-set year [ lindex [ split [ version -short ] "." ] 0 ]  
-if { [ string equal [ get_runs -quiet synth_1 ] ""] } {
-    create_run -name synth_1 -part ${xilPartName} -flow {Vivado Synthesis ${year}} -strategy "Vivado Synthesis Defaults" -constrset constrs_1
-} else {
-  set_property strategy "Vivado Synthesis Defaults" [ get_runs synth_1 ]
-    set_property flow "Vivado Synthesis ${year}" [ get_runs synth_1 ]
-}
-set obj [ get_runs synth_1 ]
-#OBSOLETE set_property "part" "xcku060-ffva1156-2-i" $obj
-
-# set the current synth run
-current_run -synthesis [ get_runs synth_1 ]
+#set year [ lindex [ split [ version -short ] "." ] 0 ]  
+#if { [ string equal [ get_runs -quiet synth_1 ] ""] } {
+#    create_run -name synth_1 -part ${xilPartName} -flow {Vivado Synthesis ${year}} -strategy "Vivado Synthesis Defaults" -constrset constrs_1
+#} else {
+#  set_property strategy "Vivado Synthesis Defaults" [ get_runs synth_1 ]
+#    set_property flow "Vivado Synthesis ${year}" [ get_runs synth_1 ]
+#}
+#set obj [ get_runs synth_1 ]
+##OBSOLETE set_property "part" "xcku060-ffva1156-2-i" $obj
+#
+## set the current synth run
+#current_run -synthesis [ get_runs synth_1 ]
 
 my_puts "################################################################################"
 my_puts "##"
@@ -291,17 +291,16 @@ my_puts "##"
 my_puts "################################################################################"
 my_puts "Start at: [clock format [clock seconds] -format {%T %a %b %d %Y}] \n"
 
-launch_runs synth_1 -jobs 8
-wait_on_run synth_1 
-#synth_design -mode default -top ${topName} -part ${xilPartName}
+#launch_runs synth_1 -jobs 8
+#wait_on_run synth_1 
+synth_design -mode default -top ${topName} -part ${xilPartName}
 
 my_puts "################################################################################"
 my_puts "##  DONE WITH SYNTHESIS RUN "
 my_puts "################################################################################"
 my_puts "End at: [clock format [clock seconds] -format {%T %a %b %d %Y}] \n"
 
-open_run synth_1 
-#-name synth_1
+#open_run synth_1 -name synth_1
 # otherwise write checkpoint will fail...
 
 write_checkpoint -force ${xprDir}/0_${topName}_static_without_role.dcp 
@@ -323,10 +322,11 @@ my_dbg_trace "Added dcp of ROLE ${roleDcpFile}." ${dbgLvl_1}
 
 set_property SCOPED_TO_CELLS {ROLE} [get_files ${roleDcpFile} ]
 
+set_property HD.RECONFIGURABLE 1 [get_cells ROLE]
 
 # Link the two dcps together
-#link_design -mode default -reconfig_partitions {ROLE}  -top ${topName} -part ${xilPartName} 
-### CAVE: link_design is done by open_design in project mode!!
+link_design -mode default -reconfig_partitions {ROLE}  -top ${topName} -part ${xilPartName} 
+
 
 # Floorplan
 create_pblock pblock_ROLE
@@ -335,56 +335,83 @@ add_cells_to_pblock pblock_ROLE [get_cells [list ROLE]] -clear_locs
 #TODO not here, in xdc instead? 
 #write_xdc ./xdc/pr.xdc --> No, xdc would be included to early
 
-set_property HD.RECONFIGURABLE 1 [get_cells ROLE]
-
-# to prevent the "out-of-date" message; we just added an alreday synthesized dcp -> not necessary
-set_property needs_refresh false [get_runs synth_1]
 
 write_checkpoint -force ${xprDir}/1_${topName}_linked.dcp
 
+###PR: Obsolete?
+# Create 'synth_1' run (if not found)
+#-------------------------------------------------------------------------------
+#set year [ lindex [ split [ version -short ] "." ] 0 ]  
+#if { [ string equal [ get_runs -quiet synth_1 ] ""] } {
+#    create_run -name synth_1 -part ${xilPartName} -flow {Vivado Synthesis ${year}} -strategy "Vivado Synthesis Defaults" -constrset constrs_1
+#} else {
+#  set_property strategy "Vivado Synthesis Defaults" [ get_runs synth_1 ]
+#    set_property flow "Vivado Synthesis ${year}" [ get_runs synth_1 ]
+#}
+#set obj [ get_runs synth_1 ]
+##OBSOLETE set_property "part" "xcku060-ffva1156-2-i" $obj
+#
+## set the current synth run
+#current_run -synthesis [ get_runs synth_1 ]
 
 # Create 'impl_1' run (if not found)
 #------------------------------------------------------------------------------- 
 
-set year [ lindex [ split [ version -short ] "." ] 0 ]  
-if { [ string equal [ get_runs -quiet impl_1 ] "" ] } {
-    create_run -name impl_1 -part ${xilPartName} -flow {Vivado Implementation ${year}} -strategy "Vivado Implementation Defaults" -constrset constrs_1 -parent_run synth_1
-} else {
-  set_property strategy "Vivado Implementation Defaults" [ get_runs impl_1 ]
-    set_property flow "Vivado Implementation ${year}" [ get_runs impl_1 ]
-}
-set obj [ get_runs impl_1 ]
-#OBSOLET set_property "part" "xcku060-ffva1156-2-i" $obj
-set_property "steps.write_bitstream.args.readback_file" "0" ${obj}
-set_property "steps.write_bitstream.args.verbose"       "0" ${obj}
+# PR is here in out of project mode -> no 'runs' possible 
+#set year [ lindex [ split [ version -short ] "." ] 0 ]  
+#if { [ string equal [ get_runs -quiet impl_1 ] "" ] } {
+#    create_run -name impl_1 -part ${xilPartName} -flow {Vivado Implementation ${year}} -strategy "Vivado Implementation Defaults" -constrset constrs_1 -parent_run synth_1
+#} else {
+#  set_property strategy "Vivado Implementation Defaults" [ get_runs impl_1 ]
+#    set_property flow "Vivado Implementation ${year}" [ get_runs impl_1 ]
+#}
+#set obj [ get_runs impl_1 ]
+##OBSOLET set_property "part" "xcku060-ffva1156-2-i" $obj
+#set_property "steps.write_bitstream.args.readback_file" "0" ${obj}
+#set_property "steps.write_bitstream.args.verbose"       "0" ${obj}
+#
+## Set the current impl run
+##-------------------------------------------------------------------------------
+#current_run -implementation [ get_runs impl_1 ]
+###PR: synth isn't necessary at this stage 
+#my_puts "################################################################################"
+#my_puts "##"
+#my_puts "##  RUN SYNTHESIS: ${xprName}  "
+#my_puts "##"
+#my_puts "################################################################################"
+#my_puts "Start at: [clock format [clock seconds] -format {%T %a %b %d %Y}] \n"
+#
+#launch_runs synth_1 -jobs 8
+#wait_on_run synth_1
 
-# Set the current impl run
-#-------------------------------------------------------------------------------
-current_run -implementation [ get_runs impl_1 ]
+#my_puts "################################################################################"
+#my_puts "##  DONE WITH SYNTHESIS RUN "
+#my_puts "################################################################################"
+#my_puts "End at: [clock format [clock seconds] -format {%T %a %b %d %Y}] \n"
+
 
 my_puts "################################################################################"
 my_puts "##"
-my_puts "##  RUN 1. IMPLEMENTATION: ${xprName}  "
+my_puts "##  RUN IMPLEMENTATION: ${xprName}  IN NON PROJECT MODE"
 my_puts "##"
 my_puts "################################################################################"
 my_puts "Start at: [clock format [clock seconds] -format {%T %a %b %d %Y}] \n"
 
 #TODO 
 #set_property strategy HighEffort [ get_runs impl_1 ]
-launch_runs impl_1 -jobs 8
-wait_on_run impl_1
+#launch_runs impl_1 -jobs 8
+#wait_on_run impl_1
 
-#opt_design
-#place_design
-#route_design
+opt_design
+place_design
+route_design
 
-open_run impl_1
 
 write_checkpoint -force ${xprDir}/2_${topName}_impl_complete.dcp
 write_checkpoint -force -cell ROLE ${xprDir}/2_${topName}_impl_${usedRole}.dcp
 
 my_puts "################################################################################"
-my_puts "##  DONE WITH 1. IMPLEMENATATION RUN; .dcp SAVED "
+my_puts "##  DONE WITH 1. IMPLEMENATATION RUN "
 my_puts "################################################################################"
 my_puts "End at: [clock format [clock seconds] -format {%T %a %b %d %Y}] \n"
 
@@ -397,13 +424,7 @@ lock_design -level routing
 
 write_checkpoint -force ${xprDir}/3_${topName}_STATIC.dcp
 
-my_puts "################################################################################"
-my_puts "## STATIC DESIGN LOCKED; .dcp SAVED" 
-my_puts "################################################################################"
-my_puts "End at: [clock format [clock seconds] -format {%T %a %b %d %Y}] \n"
-
 close_project
-
 ## TODO other ROLE configs 
 
 ########################################################################33
@@ -413,21 +434,17 @@ close_project
 open_checkpoint ${xprDir}/2_${topName}_impl_complete.dcp 
 
 ## TODO BUG? 
-#set_property UNAVAILABLE_DURING_CALIBRATION TRUE [get_ports piCLKT_Usr1Clk_p] 
-
-source ${tclDir}/fix_things.tcl
+set_property UNAVAILABLE_DURING_CALIBRATION TRUE [get_ports piCLKT_Usr1Clk_p]
 
 my_puts "################################################################################"
 my_puts "##"
-my_puts "##  RUN BITTSETREAM GENERATION: ${xprName} incl. PARTIAL BITSTREAMS "
+my_puts "##  RUN BITTSETREAM GENERATION: ${xprName}  "
 my_puts "##"
 my_puts "################################################################################"
 my_puts "Start at: [clock format [clock seconds] -format {%T %a %b %d %Y}] \n"
 
-#launch_runs impl_1 -to_step write_bitstream -jobs 8
-#wait_on_run impl_1
-
-write_bitstream -force ${xprDir}/4_${topName}.bit
+launch_runs impl_1 -to_step write_bitstream -jobs 8
+wait_on_run impl_1
 
 my_puts "################################################################################"
 my_puts "##  DONE WITH BITSTREAM GENERATION RUN "
