1b6fae632ecc29ae45a9e41f0597146990c92122
Francois Abel <fab@zurich.ibm.com>
2018-05-25 17:43:12 +0200

    Updated TCP role I/F with two ports.

--- Shell_x2Udp_x2Tcp_x2Mp_x2Mc/hls/tcp_role_if/src/tcp_role_if.cpp
+++ Shell_x2Udp_x2Tcp_x2Mp_x2Mc/hls/tcp_role_if/src/tcp_role_if.cpp
@@ -94,10 +94,35 @@ ap_uint<16> session_id_cam::compare_buffer_id(
  * \return          The returned value.
  *
  *****************************************************************************/
+ap_uint<4>  session_id_cam::compare_session_id(ap_uint<16> q_session_id)
+{
+//#pragma HLS PIPELINE II=1
+
+	for (uint8_t i=0;i<no_of_session_id_table_entries;++i){ // Go through all the entries in the filter
+#pragma HLS UNROLL
+      		if ((this->filter_entries[i].valid == 1) && (q_session_id == this->filter_entries[i].session_id)){ // Check if the entry is valid and if the addresses match
+         		return this->filter_entries[i].buffer_id; // If so delete the entry (mark as invalid)
+      		}
+   	}
+   	return -1;
+}
+
+/*****************************************************************************/
+/* NameOfTheFunction
+ *
+ * \desc            Description of function.
+ *
+ * \param[in|out|stream] <NameOfTheParameter> with short description.
+ *
+ * \return          The returned value.
+ *
+ *****************************************************************************/
 void tai_session_id_table_server(
 		stream<session_id_table_entry>& w_entry,
 		stream<ap_uint<4> > &			q_buffer_id,
-		stream<ap_uint<16> > &			r_session_id)
+		stream<ap_uint<16> > &			r_session_id,
+		stream<ap_uint<16> > &			q_session_id, //query
+		stream<ap_uint<4> > &			r_buffer_id) //response
 {
 #pragma HLS INLINE region
 #pragma HLS PIPELINE II=1 //enable_flush
@@ -116,6 +141,10 @@ void tai_session_id_table_server(
 		q_buffer_id.read(in_buffer_id);
 		in_session_id = session_id_cam_table.compare_buffer_id(in_buffer_id);
 		r_session_id.write(in_session_id);
+		//rdWrswitch = !rdWrswitch;
+	} else if(!q_session_id.empty() && !r_buffer_id.full() /*&& !rdWrswitch*/){
+		in_buffer_id = session_id_cam_table.compare_session_id(q_session_id.read());
+		r_buffer_id.write(in_buffer_id);
 		//rdWrswitch = !rdWrswitch;
 	} else if(!w_entry.empty() /*&& rdWrswitch*/){
 		w_entry.read(in_entry);
@@ -227,15 +256,32 @@ void tai_listen_port_status(
 
 	static bool listenDone = false;
 	static bool waitPortStatus = false;
+	static bool port1_2 = false; //false = port1, true = port2
 
-	// Open/Listen on Port at startup
-	if (!listenDone && !waitPortStatus && !listenPort.full()) {
+	if (port1_2) {
+		// Open/Listen on Port at startup
+		if (!listenDone && !waitPortStatus && !listenPort.full()) {
+			//listenPort.write(80);
+			listenPort.write(listen_port1);
+			waitPortStatus = true;
+		} else if (waitPortStatus && !listenPortStatus.empty()) { // Check if listening on Port was successful, otherwise try again
+			listenPortStatus.read(listenDone);
+			waitPortStatus = false;
+			port1_2=!port1_2;
+		}
+
+	} else {
+		// Open/Listen on Port at startup
+		if (!listenDone && !waitPortStatus && !listenPort.full()) {
 		//listenPort.write(80);
-		listenPort.write(listen_port1);
-		waitPortStatus = true;
-	} else if (waitPortStatus && !listenPortStatus.empty()) { // Check if listening on Port was successful, otherwise try again
-		listenPortStatus.read(listenDone);
-		waitPortStatus = false;
+			listenPort.write(listen_port2);
+			waitPortStatus = true;
+		} else if (waitPortStatus && !listenPortStatus.empty()) { // Check if listening on Port was successful, otherwise try again
+			listenPortStatus.read(listenDone);
+			waitPortStatus = false;
+			port1_2=!port1_2;
+	}
+
 	}
 }
 
@@ -327,31 +373,42 @@ void tai_check_tx_status(
 void tai_net_to_app(
 		stream<ap_uint<16> >& 	rxMetaData,
 		stream<axiWord>& 		rxData       /*, stream<ap_uint<16> >& txMetaData,*/,
-		stream<axiWord>& 		txData)
+		stream<axiWord> 		txData[NUM_TCP_SESSIONS],
+		stream<ap_uint<16> > &			q_session_id, //query
+		stream<ap_uint<4> > &			r_buffer_id)
 {
 #pragma HLS PIPELINE II=1
 
-	static enum read_write_engine { RWE_SESSION_ID = 0, RWE_DATA } read_write_engine_state;
+	static enum read_write_engine { RWE_SESSION_ID = 0, RWE_BUFFERID_WAIT, RWE_DATA } read_write_engine_state;
 	ap_uint<16> sessionID;
 	axiWord currWord;
 	static ap_uint<16> old_session_id = 0;
+	static ap_uint<4> buffer_id = 0;
 
 	switch (read_write_engine_state)
 	{
 		case RWE_SESSION_ID:
-			if (!rxMetaData.empty() /*&& !txMetaData.full()*/) {
+			if (!rxMetaData.empty() /*&& !txMetaData.full()*/ && !q_session_id.full()) {
 				rxMetaData.read(sessionID);
+				q_session_id.write(sessionID);
 				//if(old_session_id != sessionID){
 					//txMetaData.write(sessionID);
 					//old_session_id = sessionID;
 				//}
-				read_write_engine_state = RWE_DATA;
+				read_write_engine_state = RWE_BUFFERID_WAIT;
 			}
 		break;
+		case RWE_BUFFERID_WAIT:
+			if (!r_buffer_id.empty()){
+				r_buffer_id.read(buffer_id);
+
+				read_write_engine_state = RWE_DATA;
+
+			}
 		case RWE_DATA:
-			if (!rxData.empty() && !txData.full()) {
+			if (!rxData.empty() && !txData[buffer_id].full()) {
 				rxData.read(currWord);
-				txData.write(currWord);
+				txData[buffer_id].write(currWord);
 
 				if (currWord.last)
 					read_write_engine_state = RWE_SESSION_ID;
@@ -371,39 +428,148 @@ void tai_net_to_app(
  * \return          The returned value.
  *
  *****************************************************************************/
+
 void tai_app_to_buf(
-		stream<axiWord>& 		vFPGA_tx_data,
+		stream<axiWord> 		vFPGA_tx_data[NUM_TCP_SESSIONS],
 		stream<ap_uint<4> >& 	q_buffer_id,
-		stream<axiWord>& 		buff_data)
+		stream<ap_uint<16> > &	r_session_id,
+		stream<ap_uint<16> > & oTxMetaData,
+		stream<axiWord>& 		oTxData)
 {
 #pragma HLS PIPELINE II=1
 
-	static enum read_write_engine { RWE_SESS_RD = 0, RWE_BUFF_DATA} read_write_engine_state;
+	static enum read_write_engine { RWE_IDLE = 0, RWE_BUFF_ID_WR, RWE_SESS_RD_WAIT, RWE_BUFF_DATA} read_write_engine_state;
 	axiWord currWord;
+
+	static ap_uint<4> buffer_id;
+	static ap_uint<16> session_id;
+	static ap_uint<4>    	streamSource    = 0;
 
 	switch (read_write_engine_state)
 	{
-		case RWE_SESS_RD:
-			if(!vFPGA_tx_data.empty() && !q_buffer_id.full() && !buff_data.full()){
-				vFPGA_tx_data.read(currWord);
-				buff_data.write(currWord);
-				q_buffer_id.write(1);
+		case RWE_IDLE:
+			// store all input stream empty states
+			bool stream_empty[NUM_TCP_SESSIONS];
+			#pragma HLS ARRAY_PARTITION variable=stream_empty complete
+				for (uint8_t i=0;i<NUM_TCP_SESSIONS;++i) {
+					stream_empty[i] = vFPGA_tx_data[i].empty();
+			     }
+			            		// Scan for non-empty stream in circular fashion w/ round-robin
+			            		// start point -- r-r increment merged w/ tmpCtr to reduce
+			            		// combinational logic depth
+			     for (uint8_t i=0;i<NUM_TCP_SESSIONS;++i){
+			                		//uint8_t tempCtr = streamSource + 1 + i;
+			    	 uint8_t tempCtr = streamSource + i;
+			         if (tempCtr >= NUM_TCP_SESSIONS)
+			        	 tempCtr -= NUM_TCP_SESSIONS;
 
-				if(!currWord.last)
-					read_write_engine_state = RWE_BUFF_DATA;
+			         if(!stream_empty[tempCtr]){
+			        	 streamSource = tempCtr;
+			             buffer_id = streamSource;
+			             read_write_engine_state = RWE_BUFF_ID_WR;
+			             break;
+			         }
+			    }
+	     break;
+		case RWE_BUFF_ID_WR:
+			if(!q_buffer_id.full()){
+				q_buffer_id.write(buffer_id);
+
+				read_write_engine_state = RWE_SESS_RD_WAIT;
 			}
 		break;
+		case RWE_SESS_RD_WAIT:
+			if(!r_session_id.empty() && !oTxMetaData.full()){
+				r_session_id.read(session_id);
+				oTxMetaData.write(session_id);
+				read_write_engine_state = RWE_BUFF_DATA;
+			}
 		case RWE_BUFF_DATA:
-			if (!vFPGA_tx_data.empty() && !buff_data.full()) {
-				vFPGA_tx_data.read(currWord);
-				buff_data.write(currWord);
+			if (!vFPGA_tx_data[buffer_id].empty() && !oTxData.full()) {
+				vFPGA_tx_data[buffer_id].read(currWord);
+				oTxData.write(currWord);
 
 				if(currWord.last)
-					read_write_engine_state = RWE_SESS_RD;
+					read_write_engine_state = RWE_IDLE;
 			}
 		break;
 	}
 }
+
+
+#if 0
+
+void app_tx_merge(stream<axi_word> inData[NUM_PORTS],
+                  stream<ap_uint<16> > 	in_payload_length_from_app[NUM_PORTS],
+                  stream<axi_word> &outData,
+                  stream<ap_uint<16> > 	&out_payload_length_to_nts,
+                  stream<ap_uint<8> > &outPort)
+{
+#pragma HLS INLINE off
+#pragma HLS pipeline II=1 enable_flush
+
+	static enum mState{M_IDLE = 0, M_STREAM} mergeState;
+    	static ap_uint<LOG2CEIL_NUM_MERGE_STREAMS>rrCtr	= 0; 	// Counter used to ensure fair access to all the queues in the idle state
+    	static ap_uint<4>    	streamSource    = 0; 		// Denotes the stream from which the data will be read. 0 = ARP, 1 = ICMP, 2 = Loopback
+    	axi_word             	inputWord       = {0, 0, 0};
+    	ap_uint<8>           	inputPort       = 0;
+    	ap_uint<16>		curr_payload_len = 0;
+
+    	switch(mergeState){
+        	case M_IDLE:
+        	{
+            		// store all input stream empty states
+            		bool stream_empty[NUM_PORTS];
+			#pragma HLS ARRAY_PARTITION variable=stream_empty complete
+            		for (uint8_t i=0;i<NUM_PORTS;++i) {
+                		stream_empty[i] = inData[i].empty();
+            		}
+            		// Scan for non-empty stream in circular fashion w/ round-robin
+            		// start point -- r-r increment merged w/ tmpCtr to reduce
+            		// combinational logic depth
+            		for (uint8_t i=0;i<NUM_PORTS;++i){
+                		//uint8_t tempCtr = streamSource + 1 + i;
+                		uint8_t tempCtr = streamSource + i;
+                		if (tempCtr >= NUM_PORTS)
+                    			tempCtr -= NUM_PORTS;
+                		if(!stream_empty[tempCtr]){
+                    			streamSource = tempCtr;
+                    			inputWord = inData[streamSource].read();
+                    			curr_payload_len = in_payload_length_from_app[streamSource].read();
+                    			//inputPort = inPort[streamSource].read();
+                    			outData.write(inputWord);
+                    			out_payload_length_to_nts.write(curr_payload_len);
+                    			outPort.write(streamSource/2);
+                    			if (inputWord.last == 0)
+                        			mergeState = M_STREAM;
+                    			break;
+                		}
+            		}
+            	break;
+        	}
+        	case M_STREAM:
+        	{
+            		if (!inData[streamSource].empty()) {
+                		inData[streamSource].read(inputWord);
+                		outData.write(inputWord);
+                		curr_payload_len = in_payload_length_from_app[streamSource].read();
+                		out_payload_length_to_nts.write(curr_payload_len);
+                		//inPort[streamSource].read(inputPort);
+                		outPort.write(streamSource/2);
+                		if (inputWord.last == 1) {
+                    			mergeState = M_IDLE;
+                		}
+            		}
+            		break;
+        	}
+    	}
+
+
+}
+
+
+
+#endif
 
 /*****************************************************************************/
 /* NameOfTheFunction
@@ -526,8 +692,12 @@ void tcp_role_if(
 #pragma HLS resource core=AXI4Stream variable=oTxMetaData metadata="-bus_bundle m_axis_tx_meta_data"
 #pragma HLS resource core=AXI4Stream variable=oTxData metadata="-bus_bundle m_axis_tx_data"
 #pragma HLS resource core=AXI4Stream variable=iTxStatus metadata="-bus_bundle s_axis_tx_status"
-#pragma HLS resource core=AXI4Stream variable=vFPGA_rx_data metadata="-bus_bundle m_axis_vfpga_rx_data"
-#pragma HLS resource core=AXI4Stream variable=vFPGA_tx_data metadata="-bus_bundle s_axis_vfpga_tx_data"
+
+//fix with a scalable approach
+#pragma HLS resource core=AXI4Stream variable=vFPGA_rx_data metadata="-bus_bundle m_axis_vfpga_rx_data" port_map={{vFPGA_rx_data_0_V_data_V m_axis_vfpga_rx_data_TDATA_0} {vFPGA_rx_data_1_V_data_V m_axis_vfpga_rx_data_TDATA_1}\
+	{vFPGA_rx_data_0_V_keep_V m_axis_vfpga_rx_data_TKEEP_0 } {vFPGA_rx_data_1_V_keep_V m_axis_vfpga_rx_data_TKEEP_1} {vFPGA_rx_data_0_V_last_V m_axis_vfpga_rx_data_TLAST_0 } {vFPGA_rx_data_1_V_last_V m_axis_vfpga_rx_data_TLAST_1} }
+#pragma HLS resource core=AXI4Stream variable=vFPGA_tx_data metadata="-bus_bundle s_axis_vfpga_tx_data" port_map={{vFPGA_tx_data_0_V_data_V m_axis_vfpga_tx_data_TDATA_0} {vFPGA_tx_data_1_V_data_V m_axis_vfpga_tx_data_TDATA_1}\
+		{vFPGA_tx_data_0_V_keep_V m_axis_vfpga_tx_data_TKEEP_0 } {vFPGA_tx_data_1_V_keep_V m_axis_vfpga_tx_data_TKEEP_1} {vFPGA_tx_data_0_V_last_V m_axis_vfpga_tx_data_TLAST_0 } {vFPGA_tx_data_1_V_last_V m_axis_vfpga_tx_data_TLAST_1} }
 
 #pragma HLS DATA_PACK variable=iNotifications
 #pragma HLS DATA_PACK variable=oReadRequest
@@ -541,8 +711,15 @@ static stream<session_id_table_entry>  w_entry("w_entry");
 #pragma HLS STREAM variable=w_entry depth=1
 static stream<ap_uint<4> > q_buffer_id("q_buffer_id");
 #pragma HLS STREAM variable=q_buffer_id depth=1
+
+static stream<ap_uint<4> > r_buffer_id("r_buffer_id");
+#pragma HLS STREAM variable=r_buffer_id depth=1
+
 static stream<ap_uint<16> > r_session_id("r_session_id");
 #pragma HLS STREAM variable=r_session_id depth=1
+
+static stream<ap_uint<16> > q_session_id("q_session_id");
+#pragma HLS STREAM variable=q_session_id depth=1
 
 static stream<axiWord>  buff_data("buff_data");
 #pragma HLS STREAM variable=buff_data depth=1024
@@ -555,12 +732,12 @@ static stream<axiWord>  buff_data("buff_data");
 
 	tai_check_tx_status(iTxStatus);
 
-	tai_session_id_table_server(w_entry, q_buffer_id, r_session_id);
+	tai_session_id_table_server(w_entry, q_buffer_id, r_session_id, q_session_id, r_buffer_id );
 
-	tai_net_to_app(iRxMetaData, iRxData, vFPGA_rx_data[0]);
+	tai_net_to_app(iRxMetaData, iRxData, vFPGA_rx_data,q_session_id, r_buffer_id);
 
-	tai_app_to_buf(vFPGA_tx_data[0], q_buffer_id, buff_data);
-	tai_app_to_net(buff_data, oTxMetaData, oTxData, r_session_id);
+	tai_app_to_buf(vFPGA_tx_data, q_buffer_id, r_session_id, oTxMetaData, oTxData);
+	//tai_app_to_net(buff_data, oTxMetaData, oTxData, r_session_id);
 
 }
 
