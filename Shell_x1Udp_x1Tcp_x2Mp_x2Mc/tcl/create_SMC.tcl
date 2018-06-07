db0af6b98d40a25487bd2eb747227f140db99d12
Burkhard Ringlein <ngl@zurich.ibm.com>
2018-06-07 14:30:34 +0000

    adapted Roles to new type

---
+++ Shell_x1Udp_x1Tcp_x2Mp_x2Mc/tcl/create_SMC.tcl
@@ -0,0 +1,14 @@
+
+
+create_ip -name smc_main -vendor IBM -library hls -version 1.0 -module_name SMC
+set_property -dict [list CONFIG.Component_Name {SMC}] [get_ips SMC]
+
+generate_target all [get_ips SMC]
+
+create_ip_run [get_ips SMC]
+
+launch_runs -jobs 8 SMC_synth_1
+
+
+#update_compile_order -fileset sources_1
+
