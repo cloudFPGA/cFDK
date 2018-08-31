97241cefd44bb9defe9156342f20083043615c40
Burkhard Ringlein <ngl@zurich.ibm.com>
2018-08-31 14:18:57 +0000

    inital version for MPE finished, clarified and TESTED Interface, added ./ref/ to ensure correctnes of AXI4Lite

--- Shell_x1Udp_x1Tcp_x2Mp_x2Mc/hls/mpe/src/mpe.hpp
+++ Shell_x1Udp_x1Tcp_x2Mp_x2Mc/hls/mpe/src/mpe.hpp
@@ -5,6 +5,7 @@
 #include "ap_int.h"
 #include "ap_utils.h"
 #include <hls_stream.h>
+#include "../../smc/src/smc.hpp"
 
 using namespace hls;
 
@@ -34,21 +35,43 @@ using namespace hls;
 	 MPI_Interface() {}
  };
 
+ struct IPMeta {
+	 ap_uint<32> ipAddress;
+	 IPMeta() {}
+ };
+
+
+#define NUMBER_CONFIG_WORDS 10
+#define NUMBER_STATUS_WORDS 10
+
+ /*
+  * ctrlLINK Structure:
+  * 1.         0 --            NUMBER_CONFIG_WORDS -1 :  possible configuration from SMC to MPE
+  * 2. NUMBER_CONFIG_WORDS --  NUMBER_STATUS_WORDS -1 :  possible status from MPE to SMC
+  * 3. NUMBER_STATUS_WORDS --  MAX_CLUSTER_SIZE +
+  *                              NUMBER_CONFIG_WORDS +
+  *                              NUMBER_STATUS_WORDS    : Message Routing Table (MRT)
+  */
+
 void mpe_main(
-		// ----- link to SMC -----
-		ap_uint<32> *ctrlLink,
+		// ----- system reset ---
+				ap_uint<1> sys_reset,
+				// ----- link to SMC -----
+				ap_uint<32> ctrlLink[MAX_CLUSTER_SIZE + NUMBER_CONFIG_WORDS + NUMBER_STATUS_WORDS],
 
-		// ----- Nts0 / Tcp Interface -----
-		stream<Axis<64> >		&siTcp,
-		stream<Axis<64> >		&soTcp,
+				// ----- Nts0 / Tcp Interface -----
+				stream<Axis<64> >		&siTcp,
+				stream<IPMeta> 			&siIP,
+				stream<Axis<64> >		&soTcp,
+				stream<IPMeta>			&soIP,
 
-		// ----- Memory -----
-		ap_uint<8> *MEM,
+				// ----- Memory -----
+				//ap_uint<8> *MEM, TODO: maybe later
 
-		// ----- MPI_Interface -----
-		MPI_Interface *MPIif,
-		stream<Axis<8> > &MPI_data_in,
-		stream<Axis<8> > &MPI_data_out
+				// ----- MPI_Interface -----
+				stream<MPI_Interface> &siMPIif,
+				stream<Axis<8> > &siMPI_data,
+				stream<Axis<8> > &soMPI_data
 		);
 
 
