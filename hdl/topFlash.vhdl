44d179e1b31208d0feedcea55295862f53de3a55
Francois Abel <fab@zurich.ibm.com>
2018-11-13 15:29:01 +0100

    Updates default gateway register to 10.12.0.254
    Former-commit-id: 002ac35d2def195017f4f7ba2e1613b982b771d3

--- hdl/topFlash.vhdl
+++ hdl/topFlash.vhdl
@@ -66,8 +66,8 @@ entity topFlash is
     gSecurityPriviledges : string  := "super";  -- "user" or "super"
     -- Build date --------------------------------
     gTopDateYear         : stDate  := 8d"18";
-    gTopDateMonth        : stDate  := 8d"10";
-    gTopDateDay          : stDate  := 8d"15";
+    gTopDateMonth        : stDate  := 8d"11";
+    gTopDateDay          : stDate  := 8d"13";
     -- External Memory Interface (EMIF) ----------
     gEmifAddrWidth       : integer :=  8;
     gEmifDataWidth       : integer :=  8
