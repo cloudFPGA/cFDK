9fe2d9a2c686aa537e6de5c0fc8b550105664e23
Francois Abel <fab@zurich.ibm.com>
2019-02-13 11:59:15 +0100

    Implement USR_ACCESS with TIMESTAMP.
    Former-commit-id: 5e4fe7ce31e5553457044993a145fb1725c84619

--- tcl/fix_things.tcl
+++ tcl/fix_things.tcl
@@ -6,3 +6,6 @@ set_property UNAVAILABLE_DURING_CALIBRATION TRUE [get_ports piCLKT_Usr1Clk_p]
 set_property BITSTREAM.GENERAL.COMPRESS False [current_design]
 set_property bitstream.general.perFrameCRC yes [current_design]
 
+# Request to embed a timestamp into the bitstream
+set_property BITSTREAM.CONFIG.USR_ACCESS TIMESTAMP [current_design]
+
