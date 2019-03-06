c3aaf5c04fbd513afc3da82662d5269b364c3697
Burkhard Ringlein <ngl@zurich.ibm.com>
2019-03-06 17:59:26 +0000

    added consistent 256x256 testcase

--- RoleMPI/hls/mpi_wrapperv1/src/test.hpp
+++ RoleMPI/hls/mpi_wrapperv1/src/test.hpp
@@ -7,13 +7,12 @@
 
 #define USE_INTEGER
 
-#define DIM 1024
-#define LDIMX 1024
-#define LDIMY 64
+#define DIM 256
+#define LDIMX 256
+#define LDIMY 16
 
 #define PACKETLENGTH 256
 #define CORNER_VALUE_INT 128
-#define JACOBI_ITERATIONS 10
 
 #define DATA_CHANNEL_TAG 1 
 #define CMD_CHANNEL_TAG 2
