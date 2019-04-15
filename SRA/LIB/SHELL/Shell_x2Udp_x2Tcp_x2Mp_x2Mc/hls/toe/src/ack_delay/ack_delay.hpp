21a02a9927313383b7277de93e29ac31f3c6acdb
Burkhard Ringlein <ngl@zurich.ibm.com>
2019-04-15 17:22:21 +0200

    restructure 2/X

--- SRA/LIB/SHELL/Shell_x2Udp_x2Tcp_x2Mp_x2Mc/hls/toe/src/ack_delay/ack_delay.hpp
+++
@@ -1,8 +0,0 @@
-#include "../toe.hpp"
-
-using namespace hls;
-
-void ack_delay(	stream<extendedEvent>&	input,
-				stream<extendedEvent>&	output,
-				stream<ap_uint<1> >&	readCountFifo,
-				stream<ap_uint<1> >&	writeCountFifo);
