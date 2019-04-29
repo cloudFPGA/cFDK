9b31354b362611fc445bc18ddc7f292b1a7fac2a
Burkhard Ringlein <ngl@zurich.ibm.com>
2019-04-29 16:28:28 +0000

    started Themisto Shell

---
+++ SRA/LIB/SHELL/Themisto/tcl/create_ip_cores.tcl
@@ -0,0 +1,50 @@
+#  *
+#  *                       cloudFPGA
+#  *     Copyright IBM Research, All Rights Reserved
+#  *    =============================================
+#  *     Created: Apr 2019
+#  *     Authors: FAB, WEI, NGL
+#  *
+#  *     Description:
+#  *        Create TCL script for a SPECIFIC SHELL version
+#  *
+
+
+package require cmdline
+
+#source ../../../tcl/xpr_constants.tcl
+source ../../../tcl/xpr_settings.tcl
+
+set savedDir [pwd]
+# execute the LIB version
+source ../../LIB/tcl/create_ip_cores.tcl
+
+cd $savedDir 
+
+# seems not to be necessary currently...(only HLS cores in LIB)
+#set_property      ip_repo_paths [list ${ip_repo_paths} ../hls/ ] [ current_fileset ]
+#update_ip_catalog
+
+
+#------------------------------------------------------------------------------  
+# VIVADO-IP : Decouple IP 
+#------------------------------------------------------------------------------ 
+#get current port Descriptions 
+source ./decouple_ip_type.tcl 
+
+set ipModName "Decoupler"
+set ipName    "pr_decoupler"
+set ipVendor  "xilinx.com"
+set ipLibrary "ip"
+set ipVersion "1.0"
+set ipCfgList ${DecouplerType}
+
+set rc [ my_customize_ip ${ipModName} ${ipDir} ${ipVendor} ${ipLibrary} ${ipName} ${ipVersion} ${ipCfgList} ]
+
+if { ${rc} != ${::OK} } { set nrErrors [ expr { ${nrErrors} + 1 } ] }
+
+
+#now, execute the finish
+source ../../LIB/tcl/create_ip_cores_running_finish.tcl
+
+
