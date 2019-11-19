7d06f1a4db88dc46df973d068c5c093be9acaf1b
Francois Abel <fab@zurich.ibm.com>
2019-11-19 15:08:24 +0100

    Adds RXe.

--- REG/main.sh
+++ REG/main.sh
@@ -10,40 +10,58 @@
 #  *        Main regressions script. This script is called by e.g. the Jenkins server.
 #  *
 
-# ATTENTION: This script EXPECTS THAT IT IS EXECUTED FROM THE REPOISOTORY ROOT AND ALL NECESSARY ENVIROMENT IS SOURCED! 
-# $root should bet set externally
+# ATTENTION: 
+#   This script EXPECTS THAT IT IS EXECUTED FROM THE REPOSITORY ROOT AND ALL NECESSARY
+#    ENVIROMENT IS SOURCED!
+#   And $root should bet set externally.
 
-retval=1
 
-# STEP-1: Run CSim
-echo "======== START of STEP-1 ========"
+
+# @brief A function to check if previous step passed.
+# @param[in] $1 the return value of the previous command.
+function exit_on_error {
+    if [ $1 -ne 0 ]; then
+        echo "EXIT ON ERROR: Regression \'$0\' FAILED."
+        echo "  Last return value was $1."
+        exit $1
+    fi
+}
+
+
+echo "======== START of STEP-1: CSim ========"
+cd $root/SRA/LIB/SHELL/LIB/hls/toe/src/rx_engine
+make clean
+make csim
+exit_on_error $? 
+
 cd $root/SRA/LIB/SHELL/LIB/hls/toe/src/tx_engine
 make clean
 make csim
+exit_on_error $? 
+
 cd $root/SRA/LIB/SHELL/LIB/hls
 make clean
 make csim
-retval=$? # saving return value
-echo "======== END of STEP-1 ========"
+exit_on_error $? 
+echo "======== END of STEP-1: CSim ========"
 
 
 
-# check if previous step passed
-if [ $retval -ne 0 ]; then
-  echo "last return value was $retval"
-  echo "main.sh regression FAILED"
-  exit $retval
-fi
+echo "======== START of STEP-2: CoSim ========"
+cd $root/SRA/LIB/SHELL/LIB/hls/toe/src/rx_engine
+make cosim
+exit_on_error $? 
 
-# STEP-2: Run CoSim
-echo "======== START of STEP-2 ========"
 cd $root/SRA/LIB/SHELL/LIB/hls/toe/src/tx_engine
 make cosim
-retval=$? # saving return value
+exit_on_error $? 
+
 cd $root/SRA/LIB/SHELL/LIB/hls
-# [FIXME-TODO] make cosim
-echo "======== END of STEP-2 ========"
+echo ">>>>>>>> [FIXME-TODO] Must enable make cosim <<<<<<<"
+exit_on_error $? 
+
+echo "======== END of STEP-2: CoSim ========"
 
 
-exit $retval
+exit 0
 
