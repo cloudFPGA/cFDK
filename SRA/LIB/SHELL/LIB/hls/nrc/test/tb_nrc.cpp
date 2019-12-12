68b3578e06c8f3854297307e4ffc2e63acc46515
Burkhard Ringlein <ngl@zurich.ibm.com>
2019-12-12 07:35:22 +0000

    tried to remove combinatorial loop between NRC and FMC

--- SRA/LIB/SHELL/LIB/hls/nrc/test/tb_nrc.cpp
+++ SRA/LIB/SHELL/LIB/hls/nrc/test/tb_nrc.cpp
@@ -538,10 +538,10 @@ enum RxFsmStates { RX_WAIT_META=0, RX_STREAM } rxFsmState = RX_WAIT_META;
 void pFMC(
         //-- TRIF / Rx Data Interface
         stream<NetworkWord>     &siTRIF_Data,
-        stream<AppMeta>     &siTRIF_SessId,
+        stream<Axis<16> >     &siTRIF_SessId,
         //-- TRIF / Tx Data Interface
         stream<NetworkWord>     &soTRIF_Data,
-        stream<AppMeta>     &soTRIF_SessId)
+        stream<Axis<16> >     &soTRIF_SessId)
 {
     NetworkWord currWord;
     AppMeta     tcpSessId;
@@ -552,7 +552,7 @@ void pFMC(
     switch (rxFsmState ) {
     case RX_WAIT_META:
         if (!siTRIF_SessId.empty() and !soTRIF_SessId.full()) {
-            siTRIF_SessId.read(tcpSessId);
+            tcpSessId = siTRIF_SessId.read().tdata;
             soTRIF_SessId.write(tcpSessId);
             rxFsmState  = RX_STREAM;
         }
