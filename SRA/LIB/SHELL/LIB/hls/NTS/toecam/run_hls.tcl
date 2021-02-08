f358a4af90f5273050658976d0f9c681d3bc93cc
Francois Abel <fab@zurich.ibm.com>
2021-02-08 14:20:36 +0100

    NTS - Setting 'name_max_length' to 256 in all TCL scripts.

--- SRA/LIB/SHELL/LIB/hls/NTS/toecam/run_hls.tcl
+++ SRA/LIB/SHELL/LIB/hls/NTS/toecam/run_hls.tcl
@@ -103,7 +103,7 @@ config_rtl -reset control
 #  -pipeline_loops : Specify the lower threshold used during pipelining loops automatically. The
 #                    default is '0' for no automatic loop pipelining. 
 #------------------------------------------------------------------------------------------------
-config_compile -name_max_length 128 -pipeline_loops 0
+config_compile -name_max_length 256 -pipeline_loops 0
 
 #-------------------------------------------------
 # Run C Simulation (refer to UG902)
