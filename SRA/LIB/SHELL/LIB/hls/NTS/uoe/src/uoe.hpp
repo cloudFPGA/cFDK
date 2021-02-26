828cc0fa9c603a31a978cf546c3593c26cfddb33
Francois Abel <fab@zurich.ibm.com>
2021-02-26 09:55:41 +0100

    UOE - Removing AxisApp.

--- SRA/LIB/SHELL/LIB/hls/NTS/uoe/src/uoe.hpp
+++ SRA/LIB/SHELL/LIB/hls/NTS/uoe/src/uoe.hpp
@@ -43,7 +43,6 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "../../../NTS/nts.hpp"
 #include "../../../NTS/nts_utils.hpp"
 #include "../../../NTS/SimNtsUtils.hpp"
-//OBSOLETE_20210215 #include "../../AxisApp.hpp"
 #include "../../AxisIp4.hpp"
 #include "../../AxisPsd4.hpp"
 
