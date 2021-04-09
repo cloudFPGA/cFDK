414490e7a077eb499f05b5475de30afbc74d9e4a
Francois Abel <fab@zurich.ibm.com>
2021-04-10 00:02:37 +0200

    Adds the retrieval of the VIVADO-VERSION

--- SRA/LIB/tcl/xpr_settings.tcl
+++ SRA/LIB/tcl/xpr_settings.tcl
@@ -1,33 +1,34 @@
-# /*******************************************************************************
-#  * Copyright 2016 -- 2020 IBM Corporation
-#  *
-#  * Licensed under the Apache License, Version 2.0 (the "License");
-#  * you may not use this file except in compliance with the License.
-#  * You may obtain a copy of the License at
-#  *
-#  *     http://www.apache.org/licenses/LICENSE-2.0
-#  *
-#  * Unless required by applicable law or agreed to in writing, software
-#  * distributed under the License is distributed on an "AS IS" BASIS,
-#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  * See the License for the specific language governing permissions and
-#  * limitations under the License.
-# *******************************************************************************/
+# *
+# * Copyright 2016 -- 2020 IBM Corporation
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *     http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# *
 
-#  *
-#  *                       cloudFPGA
-#  *    =============================================
-#  *     Created: Apr 2019
-#  *     Authors: FAB, WEI, NGL
-#  *
-#  *     Description:
-#  *        XPR/TCL PROJECT SPECIFIC settings;
-#  *        Generic file to extract ENVIRONMENT variables
-#  *
+# ******************************************************************************
+# * 
+# * Description : A Tcl script to environment variables and to set specific
+# *                project settings.
+# *
+# ******************************************************************************
 
 
+# Retreive the Vivado version 
+#-------------------------------------------------
+set VIVADO_VERSION [file tail $::env(XILINX_VIVADO)]
 
-## IMPORT ENVIRONMENTs
+#-------------------------------------------------------------------------------
+# ENV Import
+#-------------------------------------------------------------------------------
 set cFpIpDir $env(cFpIpDir)
 set cFpMOD   $env(cFpMOD)
 set usedRoleDir $env(usedRoleDir)
@@ -37,27 +38,23 @@ set cFpRootDir $env(cFpRootDir)
 set cFpXprDir $env(cFpXprDir)
 set cFpDcpDir $env(cFpDcpDir)
 
-#source constants and functions 
+# Source constants and functions 
 source ${cFpRootDir}/cFDK/SRA/LIB/tcl/xpr_constants.tcl
-# source board specific settings 
+# Source board specific settings 
 source ${cFpRootDir}/cFDK/MOD/${cFpMOD}/tcl/xpr_settings.tcl
 
 #-------------------------------------------------------------------------------
-# TOP  Project Settings
+# TOP Project Settings
 #-------------------------------------------------------------------------------
-#set currDir      [pwd]
 set rootDir      ${cFpRootDir} 
-#set hdlDir       ${rootDir}/hdl
-#set hlsDir       ${rootDir}/hls
 set ipDir        ${cFpIpDir}
-#set tclDir       ${rootDir}/tcl
 set tclTopDir    ${cFpRootDir}/TOP/tcl/
 set xdcDir       ${cFpRootDir}/cFDK/MOD/${cFpMOD}/xdc/
 set xprDir       ${cFpXprDir}
 set dcpDir       ${cFpDcpDir}
 
 # Not used: set ipXprDir     ${ipDir}/managed_ip_project
-# Not used:set ipXprName    "managed_ip_project"
+# Not used: set ipXprName    "managed_ip_project"
 # Not used: set ipXprFile    [file join ${ipXprDir} ${ipXprName}.xpr ]
 
 #-------------------------------------------------------------------------------
@@ -74,7 +71,6 @@ set usedShellType "Shell_${cFpSRAtype}"
 #-------------------------------------------------------------------------------
 # Debug outpus
 #-------------------------------------------------------------------------------
-
 my_info_puts "RootDir: ${rootDir}"
 my_info_puts "ipDir: ${ipDir}"
 my_info_puts "xdcDir: ${xdcDir}"
