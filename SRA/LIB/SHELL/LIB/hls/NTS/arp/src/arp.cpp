d6d33c015dee04b4e8d3e3735e5ef51e2c11fe2d
Francois Abel <fab@zurich.ibm.com>
2021-04-12 21:52:20 +0200

    ARP - Adds missing DATA_PACK

--- SRA/LIB/SHELL/LIB/hls/NTS/arp/src/arp.cpp
+++ SRA/LIB/SHELL/LIB/hls/NTS/arp/src/arp.cpp
@@ -679,13 +679,17 @@ void arp(
     #pragma HLS INTERFACE axis off           port=siIPRX_Data
     #pragma HLS INTERFACE axis off           port=soETH_Data
 
-	#pragma HLS INTERFACE axis register both port=siIPTX_MacLkpReq
-	#pragma HLS INTERFACE axis register both port=soIPTX_MacLkpRep
+    #pragma HLS INTERFACE axis register both port=siIPTX_MacLkpReq
+    #pragma HLS INTERFACE axis register both port=soIPTX_MacLkpRep
 
-	#pragma HLS INTERFACE axis register both port=soCAM_MacLkpReq
-	#pragma HLS INTERFACE axis register both port=siCAM_MacLkpRep
+    #pragma HLS INTERFACE axis register both port=soCAM_MacLkpReq
+    #pragma HLS DATA_PACK                variable=soCAM_MacLkpReq
+    #pragma HLS INTERFACE axis register both port=siCAM_MacLkpRep
+    #pragma HLS DATA_PACK                variable=siCAM_MacLkpRep
     #pragma HLS INTERFACE axis register both port=soCAM_MacUpdateReq
-	#pragma HLS INTERFACE axis register both port=siCAM_MacUpdateRep
+    #pragma HLS DATA_PACK                variable=soCAM_MacUpdateReq
+    #pragma HLS INTERFACE axis register both port=siCAM_MacUpdateRep
+    #pragma HLS DATA_PACK                variable=siCAM_MacUpdateRep
 
     //-- DIRECTIVES FOR THIS PROCESS -------------------------------------------
     #pragma HLS DATAFLOW disable_start_propagation
