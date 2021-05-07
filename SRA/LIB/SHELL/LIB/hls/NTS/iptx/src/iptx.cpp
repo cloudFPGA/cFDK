877949f2a83c5c9f53dc2db9df9c510f44ac873a
Francois Abel <fab@zurich.ibm.com>
2021-05-07 21:06:33 +0200

    Cosmetic

--- SRA/LIB/SHELL/LIB/hls/NTS/iptx/src/iptx.cpp
+++ SRA/LIB/SHELL/LIB/hls/NTS/iptx/src/iptx.cpp
@@ -687,7 +687,7 @@ void iptx(
 
     #pragma HLS INTERFACE axis register both port=soARP_LookupReq
     #pragma HLS INTERFACE axis off           port=siARP_LookupRep
-    #pragma HLS DATA_PACK              variable=siARP_LookupRep
+    #pragma HLS DATA_PACK                variable=siARP_LookupRep
 
     //-- DIRECTIVES FOR THIS PROCESS -------------------------------------------
     #pragma HLS DATAFLOW disable_start_propagation
