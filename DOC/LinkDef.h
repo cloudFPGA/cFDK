65a4358cd8211c5763e32218c780b96abc797c63
Dionysios Diamantopoulos <did@zurich.ibm.com>
2021-11-25 22:52:30 +0100

    Updating DOC/LinkDef.h with cFp_Zoo.

--- DOC/LinkDef.h
+++ DOC/LinkDef.h
@@ -44,8 +44,10 @@
  *              |       |  [+]  NTS_UOE_TEST UOE_TEST
  *              |       +-- NTS_SIM SimNts
  *              +--[+] SRA SRA
- *                  +--[+] Kale Kale
- *                  +--[+] Themisto Themisto
+ *              |   +--[+] Kale Kale
+ *              |   +--[+] Themisto Themisto
+ *              +--[+] cFp cFp
+ *              |   +--[+] cFp_Zoo cFp_Zoo
  *
  *****************************************************************************/
 
@@ -78,6 +80,12 @@
  *  @ingroup cFDK
  * 
  *  \brief Shell-Role-Architecture (SRA). To abstract the details of the hardware from the user and to assert certain levels of security, cloudFPGA uses a Shell-Role-Architecture (SRA). The documentation of SRA is available at https://pages.github.ibm.com/cloudFPGA/Doc/pages/cfdk.html#sras .
+ */
+
+/** \defgroup cFp cFp
+ *  @ingroup cFDK
+ * 
+ *  \brief cloudFPGA Projects (cFp). This is a group with existing projects developed with cFDK and can be used as templates for new projects.
  */
 
 /*****************************************************************************
@@ -254,5 +262,109 @@
  *  @ingroup NTS
  * 
  *  \brief Support and utilities for the simulation of the Network Transport Stack (NTS).
+ */
+
+
+/*****************************************************************************
+ *
+ *  cFDK / cFp : Submodules
+ *
+ *****************************************************************************/
+
+/** \defgroup cFp_Zoo cFp_Zoo
+ *  @ingroup cFp
+ * 
+ *  \brief This project is based on Themisto Shell and it features several domain-specific accelerators.
+ */
+
+/*****************************************************************************
+ *
+ *  cFDK / cFp / cFp_Zoo : Submodules
+ *
+ *****************************************************************************/
+
+/** \defgroup VitisVision cF Vitis Vision
+ *  @ingroup cFp_Zoo
+ *
+ * \brief This is the accelerated functions of Vitis Vision sub-library from Vitis Open Source Library.
+ */
+
+/** \defgroup VitisQuantitativeFinance cF Vitis Quantitative Finance
+ *  @ingroup cFp_Zoo
+ *
+ * \brief This is the accelerated functions of Vitis Quantitative Finance sub-library from Vitis Open Source Library.
+ */
+
+
+/*****************************************************************************
+ *
+ *  cFDK / cFp / cFp_Zoo / VitisVision : Submodules
+ *
+ *****************************************************************************/
+
+
+//*********** Harris*****************************
+/** \defgroup Harris Harris
+ *  @ingroup VitisVision
+ *
+ * \brief This is the Harris accelerated function from Vitis Vision Open Source Library.
+ */
+
+/** \defgroup HarrisTB Harris Testbench
+ *  @ingroup Harris
+ *
+ * \brief This is a subgroup of Harris accelerated function with only testbench-related functions/classes/.
+ */
+
+/** \defgroup HarrisHLS Harris Vivado HLS
+ *  @ingroup Harris
+ *
+ * \brief This is a subgroup of Harris accelerated function with only synthesizable (Vivado HLS) functions/classes.
+ */
+
+//*********** Gammacorrection ********************
+/** \defgroup Gammacorrection Gammacorrection
+ *  @ingroup VitisVision
+ *
+ * \brief This is the Gammacorrection accelerated function from Vitis Vision Open Source Library.
+ */
+
+/** \defgroup GammacorrectionTB Gammacorrection Testbench
+ *  @ingroup Gammacorrection
+ *
+ * \brief This is a subgroup of Gammacorrection accelerated function with only testbench-related functions/classes/.
+ */
+
+/** \defgroup GammacorrectionHLS Gammacorrection Vivado HLS
+ *  @ingroup Gammacorrection
+ *
+ * \brief This is a subgroup of Gammacorrection accelerated function with only synthesizable (Vivado HLS) functions/classes.
+ */
+
+
+/*****************************************************************************
+ *
+ *  cFDK / cFp / cFp_Zoo / VitisQuantitativeFinance : Submodules
+ *
+ *****************************************************************************/
+
+
+//*********** MCEuropeanEngine*****************************
+/** \defgroup MCEuropeanEngine MCEuropeanEngine
+ *  @ingroup VitisQuantitativeFinance
+ *
+ * \brief This is the MCEuropeanEngine accelerated function from Vitis Quantitative Finance Open Source Library.
+ */
+
+/** \defgroup MCEuropeanEngineTB MCEuropeanEngine Testbench
+ *  @ingroup MCEuropeanEngine
+ *
+ * \brief This is a subgroup of MCEuropeanEngine accelerated function with only testbench-related functions/classes/.
+ */
+
+/** \defgroup MCEuropeanEngineHLS MCEuropeanEngine Vivado HLS
+ *  @ingroup MCEuropeanEngine
+ *
+ * \brief This is a subgroup of MCEuropeanEngine accelerated function with only synthesizable (Vivado HLS) functions/classes.
  */
 
