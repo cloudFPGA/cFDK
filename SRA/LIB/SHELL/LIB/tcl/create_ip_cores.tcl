75b06601264feb7a75c75b6a07502c08a5c43a8e
Burkhard Ringlein <ngl@zurich.ibm.com>
2022-01-10 20:22:02 +0000

    Revert "adding extra routines for encrypted ip cores"
    This reverts commit 05cb620a47d003a0863c8a3db4575488480fa76b.

--- SRA/LIB/SHELL/LIB/tcl/create_ip_cores.tcl
+++ SRA/LIB/SHELL/LIB/tcl/create_ip_cores.tcl
@@ -448,7 +448,7 @@ set rc [ my_customize_ip ${ipModName} ${ipDir} ${ipVendor} ${ipLibrary} ${ipName
 if { ${rc} != ${::OK} } { set nrErrors [ expr { ${nrErrors} + 1 } ] }
 
 ###############################################################################
-##
+##                                   
 ##  PHASE-2: Creating Vivado-based IPs
 ##
 ###############################################################################
@@ -679,6 +679,54 @@ set ipCfgList  [ list CONFIG.TDATA_NUM_BYTES {13} \
 set rc [ my_customize_ip ${ipModName} ${ipDir} ${ipVendor} ${ipLibrary} ${ipName} ${ipVersion} ${ipCfgList} ]
 if { ${rc} != ${::OK} } { set nrErrors [ expr { ${nrErrors} + 1 } ] }
 
+#------------------------------------------------------------------------------  
+# VIVADO-IP : 10G Ethernet Subsystem [ETH0, 10GBASE-R] 
+#------------------------------------------------------------------------------
+#  Ethernet Standard
+#    [BASE-R]    : PCS/PMA Standard
+#    [64 bit]    : AXI4-Stream data path width
+#  MAC Options
+#    Management  Options
+#      [NO]      : AXI4-Lite for Configuration and Status
+#      [200.00]  : AXI4-Lite Frequency (MHz)
+#      [NO]}     : Statistic Gathering
+#  Flow Control Options
+#      [NO]      : IEEE802.1QBB Priority-based Flow Control
+#  PCS/PMA Options
+#    PCS/PMA Options
+#      [NO]      : Auto-Negociation
+#      [NO]      : Forward Error Correction (FEC)
+#      [NO]      : Exclude Rx Elastic Buffer
+#    DRP Clocking
+#      [156.25]  : Frequency (MHz)
+#    Transceiver : Clocking and Location
+#      [X1Y8]    : Transceiver Location
+#      [refclk0] : Transceiver RefClk Location
+#      [156.25]  : Reference Clock Frequency (MHz)
+#    Transceiver Debug   
+#      [NO]      : Additionnal transceiver DRP ports
+#      [YES]     : Additionnal transceiver control and status ports
+#  IEEE1588 Options
+#    [None]      : IEEE1588 hardware timestamping support
+#  Shared Logic
+#    [InCore]    : Include Shared Logic in core
+#------------------------------------------------------------------------------
+set ipModName "TenGigEthSubSys_X1Y8_R"
+set ipName    "axi_10g_ethernet"
+set ipVendor  "xilinx.com"
+set ipLibrary "ip"
+set ipVersion "3.1"
+set ipCfgList  [ list CONFIG.Management_Interface {false} \
+                      CONFIG.base_kr {BASE-R} \
+                      CONFIG.DClkRate {156.25} \
+                      CONFIG.SupportLevel {1} \
+                      CONFIG.Locations {X1Y8} \
+                      CONFIG.autonegotiation {0} \
+                      CONFIG.TransceiverControl {true} \
+                      CONFIG.fec {0} \
+                      CONFIG.Statistics_Gathering {0} ]
+set rc [ my_customize_ip ${ipModName} ${ipDir} ${ipVendor} ${ipLibrary} ${ipName} ${ipVersion} ${ipCfgList} ]
+if { ${rc} != ${::OK} } { set nrErrors [ expr { ${nrErrors} + 1 } ] }
 
 #------------------------------------------------------------------------------  
 # VIVADO-IP : AXI DataMover [M512, S64, B16] 
@@ -1016,13 +1064,3 @@ set ipCfgList [list CONFIG.C_WRITE_FIFO_DEPTH {1024} \
                     CONFIG.C_ICAP_EXTERNAL {0} ] 
 set rc [ my_customize_ip ${ipModName} ${ipDir} ${ipVendor} ${ipLibrary} ${ipName} ${ipVersion} ${ipCfgList} ]
 if { ${rc} != ${::OK} } { set nrErrors [ expr { ${nrErrors} + 1 } ] }
-
-###############################################################################
-##
-##  PHASE-3: Creating Vivado-based ENCRYPTED IPs
-##
-###############################################################################
-my_puts ""
-
-source ${rootDir}/cFDK/SRA/LIB/SHELL/LIB/tcl/enc_ips_list.tcl
-
